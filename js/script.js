87acae9582c0e79a0602db82a3edf205ca07b814
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-24 12:07:45 +0300

    my commit  validate the form & add alert messages

--- js/script.js
+++ js/script.js
@@ -15,4 +15,20 @@ $(document).ready(function(){
      $(".img_wrap").hover(function () {
         $(this).find("div").toggleClass();
       });
+
+      $("#submit").click(function () {
+        event.preventDefault()
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+  
+        if (name == '' || email == '' || message == '') {
+          $("#post").html("Please fill all the fields");
+          $(".post").css('background-color', 'red')
+        } else {
+          $("#post").html(name + " we have received your message. Thank you for your feedback.")
+          $(".post").css('background-color', 'green')
+          $("form")[0].reset();
+        }
+      });
 });
